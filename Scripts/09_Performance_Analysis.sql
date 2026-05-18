bd0e762318979ac8b0509109661ed491c9c2073f
Manish Raj <95201321+Hsinam007@users.noreply.github.com>
2026-05-18 09:07:51 +0530

    Update 09_Performance_Analysis.sql

--- Scripts/09_Performance_Analysis.sql
+++ Scripts/09_Performance_Analysis.sql
@@ -1,3 +1,19 @@
+
+/*
+===============================================================================
+Performance Analysis (Year-over-Year, Month-over-Month)
+===============================================================================
+Purpose:
+    - To measure the performance of products, customers, or regions over time.
+    - For benchmarking and identifying high-performing entities.
+    - To track yearly trends and growth.
+
+SQL Functions Used:
+    - LAG(): Accesses data from previous rows.
+    - AVG() OVER(): Computes average values within partitions.
+    - CASE: Defines conditional logic for trend analysis.
+===============================================================================
+*/
 -- 9.1 Gross margin by product (sales - cost) / sales
   SELECT
     TOP 10
@@ -56,6 +72,10 @@
   GROUP BY YEAR(order_date)
   ORDER BY year;
 
+
+
+/* Analyze the yearly performance of products by comparing their sales 
+to both the average sales performance of the product and the previous year's sales */
 
 WITH yearly_product_sales AS (
     SELECT
